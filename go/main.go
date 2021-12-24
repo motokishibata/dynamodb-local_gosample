acf9c8bf0a948340c9d1315376c5703d70a312c4
shibata <motok.shibat@gmail.com>
2021-12-24 10:55:41 +0900

    init

---
+++ go/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"log"
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"	
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+)
+
+func main() {
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithEndpointResolver(aws.EndpointResolverFunc(
+			func(service, region string) (aws.Endpoint, error) {
+				return aws.Endpoint{URL: "http://dynamodb-local:8000"}, nil
+			})),
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	svc := dynamodb.NewFromConfig(cfg)
+
+	resp, err := svc.ListTables(context.TODO(), &dynamodb.ListTablesInput{
+		Limit: aws.Int32(5),
+	})
+	if err != nil {
+		log.Fatalf("failed to list tables, %v", err)
+	}
+
+	fmt.Println("Tables:")
+	for _, tableName := range resp.TableNames {
+		fmt.Println(tableName)
+	}
+}
